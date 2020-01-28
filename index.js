efd0deacf3295b9b8c7c5beccd08ad4f61e5356e
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-01-28 12:50:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var blurb = [];
-
+  for (var i = 0; i < musicians.length; i++) {
+    blurb[i] = musicians[i] + " plays " + instruments[i];
+  }
 }
 
 

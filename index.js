1f766bdad9eb97cac9b8854041e3243e3637c9b5
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-01-28 12:46:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var blurb = [];
+  
+}
+
+
+
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian";
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
